efd98dcd022fd3ca915bdc6727f1932061d37979
morenaCh <bozena.chilecka@gmail.com>
2019-03-14 21:06:23 +0100

    changes task 16

--- addressbook-webtests/src/test/java/ru/stq/pft/addressbook/tests/BaseTest.java
+++ addressbook-webtests/src/test/java/ru/stq/pft/addressbook/tests/BaseTest.java
@@ -1,15 +1,14 @@
 package ru.stq.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stq.pft.addressbook.appmanager.ApplicationManager;
 
 public class BaseTest {
 
-    public static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    public static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
